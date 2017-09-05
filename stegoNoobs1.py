9b77df315275cd6dad7446421cab380c6b7334cc
earth38 <wuthering.heights38@gmail.com>
2017-09-05 21:09:45 +0900

    Add files via upload

---
+++ stegoNoobs1.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+from PIL import Image
+#画像の読み込み
+#im = Image.open("01_white.png")
+im = Image.open("01_white.png")
+#RGBに変換
+rgb_im = im.convert('RGB')
+#画像サイズを取得
+size = rgb_im.size
+#loop
+#x
+for x in range(size[0]):
+    #y
+    for y in range(size[1]):
+        #ピクセルを取得
+        r,g,b = rgb_im.getpixel((x,y))
+        print(str(r) + "," + str(g) + "," + str(b))
