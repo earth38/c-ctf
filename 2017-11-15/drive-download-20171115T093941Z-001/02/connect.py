371b70bdd8d9721e32b1e3aa388a1a7b1086c403
earth38 <wuthering.heights38@gmail.com>
2017-11-19 12:10:58 +0900

    first commit

---
+++ 2017-11-15/drive-download-20171115T093941Z-001/02/connect.py
@@ -0,0 +1,40 @@
+#coding:utf-8
+from PIL import Image
+
+with open("picture") as f:
+    lines = f.readlines()
+    lines = [x.strip() for x in lines]
+
+
+for (i,x) in enumerate(lines):
+#for line in lines:
+    #画像の読み込み
+    im = Image.open(x) 
+    #im = Image.open(line)
+    #RGBに変換
+    rgb_im = im.convert('RGB')
+    #画像サイズを取得
+    size = rgb_im.size
+    #取得したサイズと同じ空のイメージを新規に作成
+    if i == 0:
+        im2 = Image.new('RGBA',size)
+    #loop
+    for x in range(size[0]):
+        #y
+        for y in range(size[1]):
+            #ピクセルを取得
+            r,g,b = rgb_im.getpixel((x,y))
+            #反転処理
+            #if g==0:
+            #    r = 255
+            #    g = 255
+            #    b = 255
+            #set pixel
+            if i == 0:
+                im2.putpixel((x,y),(r,g,b,0))
+            elif g != 0:
+                im2.putpixel((x,y),(r,g,b,0))
+            else:  
+                pass
+    #show
+im2.show()
